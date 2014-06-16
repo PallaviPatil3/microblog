52051af1b5795ed4b6492a32c323dfa9b6df14df
PallaviPatil3 <pallavip312@gmail.com>
2014-06-16 15:49:07 +0530

    Modify Mailers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 	def create
 		super do |resource|
-			UserMailer.welcome_email(@user).deliver
+			#UserMailer.welcome_email(@user).deliver
 		end
 	end
 
 	def update
 		super do |resource|
-			UserMailer.changed_email(@user).deliver
+			#UserMailer.changed_email(@user).deliver
 		end
 	end
 end
